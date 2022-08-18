5b8024a338ce356af63d2ff5bcf2717d3635fb39
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-08-18 15:22:32 +0300

    Make header component

--- workshop-pet-app/src/App.js
+++ workshop-pet-app/src/App.js
@@ -1,27 +1,11 @@
-
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
         <div id="container">
         
-        <header id="site-header">
-            <nav className="navbar">
-                <section className="navbar-dashboard">
-                    <a href="#">Dashboard</a>
-                    <div id="guest">
-                        <a className="button" href="#">Login</a>
-                        <a className="button" href="#">Register</a>
-                    </div>
-                    <div id="user">
-                        <span>Welcome,</span>
-                        <a className="button" href="#">My Pets</a>
-                        <a className="button" href="#">Add Pet</a>
-                        <a className="button" href="#">Logout</a>
-                    </div>
-                </section>
-            </nav>
-        </header>
+        <Header/>
 
         <main id="site-content"></main>
 
