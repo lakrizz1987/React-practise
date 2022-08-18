b84c0b75bc4b80c56d3f892a97e91bc65ebf4e47
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-08-18 15:27:05 +0300

    create dashboard and details components

--- workshop-pet-app/src/App.js
+++ workshop-pet-app/src/App.js
@@ -13,66 +13,9 @@ function App() {
 
        
 
-        <section id="dashboard-page" className="dashboard">
-            <h1>Dashboard</h1>
-            <ul className="other-pets-list">
-                <li className="otherPet">
-                    <h3>Name: Buddy</h3>
-                    <p>Type: dog</p>
-                    <p className="img"><img src="/images/dog2.png"/></p>
-                    <a className="button" href="#">Details</a>
-                </li>
+        
 
-                <li className="otherPet">
-                    <h3>Name: Tyson</h3>
-                    <p>Type: parrot</p>
-                    <p className="img"><img src="/images/parrot.png"/></p>
-                    <a className="button" href="#">Details</a>
-                </li>
-
-                <li className="otherPet">
-                    <h3>Name: Milo</h3>
-                    <p>Type: dog</p>
-                    <p className="img"><img src="/images/dog.png"/></p>
-                    <a className="button" href="#">Details</a>
-                </li>
-
-                <li className="otherPet">
-                    <h3>Name: Tom</h3>
-                    <p>Type: cat</p>
-                    <p className="img"><img src="/images/cat1.png"/></p>
-                    <a className="button" href="#">Details</a>
-                </li>
-            </ul>
-            <p className="no-pets">No pets in database!</p>
-        </section>
-
-        <section id="details-page" className="details">
-            <div className="pet-information">
-                <h3>Name: Milo</h3>
-                <p className="type">Type: dog</p>
-                <p className="img"><img src="/images/dog.png"/></p>
-                <div className="actions">
-                    <a className="button" href="#">Edit</a>
-                    <a className="button" href="#">Delete</a>
-                    
-                    <a className="button" href="#">Like</a>
-                    
-                    <div className="likes">
-						<img className="hearts" src="/images/heart.png"/>
-						<span id="total-likes">Likes: 0</span>
-					</div>
-                </div>
-            </div>
-            <div className="pet-description">
-                <h3>Description:</h3>
-                <p>Today, some dogs are used as pets, others are used to help humans do their work. They are a popular
-                    pet because they are usually playful, friendly, loyal and listen to humans. Thirty million dogs in
-                    the United States are registered as pets.[5] Dogs eat both meat and vegetables, often mixed together
-                    and sold in stores as dog food. Dogs often have jobs, including as police dogs, army dogs,
-                    assistance dogs, fire dogs, messenger dogs, hunting dogs, herding dogs, or rescue dogs.</p>
-            </div>
-        </section>
+        
 
         <section id="create-page" className="create">
             <form id="create-form" action="" method="">
