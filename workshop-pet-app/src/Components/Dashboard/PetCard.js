b1314772b252f9c7d0aa946c71cfd9db5c39411f
Ivaylo Ignatov <ignatov.ivaylo@gmail.com>
2022-08-18 16:46:03 +0300

    commit

--- workshop-pet-app/src/Components/Dashboard/PetCard.js
+++ workshop-pet-app/src/Components/Dashboard/PetCard.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom'
+
 export default function PetCard({pet}){
     return(
         <li className="otherPet">
                 <h3>Name: {pet.name}</h3>
                 <p>Type: {pet.type}</p>
                 <p className="img"><img src={pet.img}/></p>
-                <a className="button" href="#">Details</a>
+                <Link className="button" to={pet._id}>Details</Link>
             </li>
     )
 }
