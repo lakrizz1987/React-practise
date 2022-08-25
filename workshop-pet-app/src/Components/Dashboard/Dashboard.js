d3b4f7ed7a664cd34eb89d5367a035bf7dd0836b
Ivaylo <ignatov.ivaylo@gmail.com>
2022-08-25 15:23:47 +0300

    create spinner

--- workshop-pet-app/src/Components/Dashboard/Dashboard.js
+++ workshop-pet-app/src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,10 @@
 import PetCard from "./PetCard";
 import { useState, useEffect } from 'react';
 import { getAll } from "../../services/services";
-
+import Loader from '../Loader/Loader'
 
 export default function Dashboard() {
-    const [pets, setPets] = useState([]);
+    const [pets, setPets] = useState('');
 
     useEffect(() => {
         getAll()
@@ -16,13 +16,13 @@ export default function Dashboard() {
         <section id="dashboard-page" className="dashboard">
             <h1>Dashboard</h1>
             <ul className="other-pets-list">
-                {(pets.length > 0)
+                {pets ? (pets.length > 0)
                     ?
                     pets.map(pet => <PetCard key={pet._id} pet={pet} />)
                     :
-                    <p className="no-pets">No pets in database!</p>
-                }
+                    <p className="no-pets">No pets in database!</p> : <Loader />}
             </ul>
+
         </section>
     )
 } 
